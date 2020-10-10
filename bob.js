bbc25e7907427a2105d9b2a0a394534b63673c5e
Jaskaran Simk <67783735+Jaskaran-Simk@users.noreply.github.com>
2020-10-10 15:47:14 +0530

    Update bob.js

--- bob.js
+++ bob.js
@@ -2,7 +2,7 @@ class Bob {
     constructor(x,y, radius){    
     var options ={
         isStatic: false,
-        restitution: 0.1,
+        restitution: 0.9,
         friction: 0,
         density: 2.2
      }
